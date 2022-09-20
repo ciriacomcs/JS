bf8ddf71bf56a72afe060aabcf1f7ddba9648e46
Moisés Ciríaco <87102272+ciriacomcs@users.noreply.github.com>
2022-09-20 19:50:36 -0300

    Initial commit

---
+++ aula16/funcao02.js
@@ -0,0 +1,6 @@
+function soma(n1=0, n2=0) {   // (n1=0, n2=0) -- aqui definimos os parâmetros para n1 e n2, caso ele não seja definido na chamada da função
+    return n1 + n2
+}
+
+// let res = soma(3, 4)  ------> pode ser usado dessa forma ou a forma escrita na linha abaixo
+console.log(soma(3, 4))
